d5163abd809dedf361f1526209a6db350344d1b9
Anindya Dolui <105700454+aninxya07@users.noreply.github.com>
2025-01-20 04:25:36 +0530

    fifth commit

--- src/store/useCodeEditorStore.ts
+++ src/store/useCodeEditorStore.ts
@@ -7,14 +7,14 @@ const getInitialState = () => {
   // if we're on the server, return default values
   if (typeof window === "undefined") {
     return {
-      language: "javascript",
+      language: "cpp",
       fontSize: 16,
       theme: "vs-dark",
     };
   }
 
   // if we're on the client, return values from local storage bc localStorage is a browser API.
-  const savedLanguage = localStorage.getItem("editor-language") || "javascript";
+  const savedLanguage = localStorage.getItem("editor-language") || "cpp";
   const savedTheme = localStorage.getItem("editor-theme") || "vs-dark";
   const savedFontSize = localStorage.getItem("editor-font-size") || 16;
 
